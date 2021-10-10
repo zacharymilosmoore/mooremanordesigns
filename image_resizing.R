5e59c30600dceddf5d949f84d90c0fa591187758
Zack <zacharymilosmoore@gmail.com>
2021-10-10 15:28:13 -0400

    remove and replace photos 1

--- image_resizing.R
+++ image_resizing.R
@@ -12,22 +12,27 @@ library(imager)
     #Only updating those without existing thumbnails to save time
     images<-images[which(file.exists(paste("portfolio_thumb/",images,sep=""))==T)]
   
-  input<-"portfolio/"
+  input<-"portfolio_org/"
   output<-"portfolio_thumb/"
+  output2<-"portfolio/"
 
 #Loop Through Images
   
   for(i in images){
     input_name<-paste(input,i,sep="")
     output_name<-paste(output,i,sep="")
+    output2_name<-paste(output2,i,sep="")
 
     im<-load.image(input_name)
       w<-width(im)
       h<-height(im)
-      s<- if(h<1000) {2} else {10}  
+      s<- if(h<1000) {1} else {10}  
       
     thmb<-resize(im,round(w/s),round(h/s))
-
+    alt<-resize(im,round(0.99*w),round(0.99*h))
+    
     save.image(thmb,output_name)
+    save.image(alt,output2_name)
+    
   }
   
